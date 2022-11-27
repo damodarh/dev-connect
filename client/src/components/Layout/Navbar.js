18e962620800ec1e576167bac4ec46c0bafd9d2c
Damodar <dhegde50@gmail.com>
2022-11-26 23:16:43 -0600

    Added dashboard icon and dashboard redirect

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -8,7 +8,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
-                <Link onClick={logout} to='/'>
+                <Link onClick={logout} to='/dashboard'>
+                    <i className='fas fa-user'></i>{' '}
+                    <span className='hide-sm'>Dashboard</span>
+                </Link>
+            </li>
+            <li>
+                <Link onClick={logout} to='#!'>
                     <i className='fas fa-sign-out-alt'></i>{' '}
                     <span className='hide-sm'>Logout</span>
                 </Link>
