cc81111184fceec8526ec9f979fa68036250cea1
Damodar <dhegde50@gmail.com>
2022-11-26 23:35:11 -0600

    Added spinner

---
+++ client/src/components/Layout/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+    return (
+        <Fragment>
+            <img 
+            src={spinner}
+            style={{ width: '50px', margin: 'auto', display: 'block' }}
+            alt='Loading...' />
+        </Fragment>
+    );
+};
+
+export default Spinner;
