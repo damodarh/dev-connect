b0259b30390831686718461080c9113cf7400498
Damodar <dhegde50@gmail.com>
2022-11-26 23:45:57 -0600

    Added clear profile action and setup for profile creation

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
+    CLEAR_PROFILE,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -84,6 +85,9 @@ export const login = (email, password) => async (dispatch) => {
 
 //Logout / Clear profile
 export const logout = () => (dispatch) => {
+    dispatch({
+        type: CLEAR_PROFILE,
+    });
     dispatch({
         type: LOGOUT,
     });
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,12 +3,38 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../Layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import { Link } from 'react-router-dom';
 
-const Dashboard = ({ getCurrentProfile, auth, profile: {profile, loading} }) => {
+const Dashboard = ({
+    getCurrentProfile,
+    auth: { user },
+    profile: { profile, loading },
+}) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
-    return loading && profile === null ? <Spinner /> : <Fragment>test</Fragment>;
+    return loading && profile === null ? (
+        <Spinner />
+    ) : (
+        <Fragment>
+            <h1 className='large text-primary'>Dashboard</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i>Welcome {user && user.name}
+            </p>
+            {profile !== null ? (
+                <>has</>
+            ) : (
+                <>
+                    <p>
+                        You have not yet setup a profile, please add some info
+                    </p>
+                    <Link to='/create-profile' className='btn btn-primary my-1'>
+                        Create Profile
+                    </Link>
+                </>
+            )}
+        </Fragment>
+    );
 };
 
 Dashboard.propTypes = {
