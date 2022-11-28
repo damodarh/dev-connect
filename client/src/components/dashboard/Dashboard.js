8dc1124e75db567817e4f12f5137700b90f6afa7
Damodar <dhegde50@gmail.com>
2022-11-27 21:12:53 -0600

    Added education and experience display components

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import Spinner from '../Layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
     getCurrentProfile,
@@ -25,6 +27,8 @@ const Dashboard = ({
             {profile !== null ? (
                 <>
                     <DashboardActions />
+                    <Experience experience={profile.experience} />
+                    <Education education={profile.education} />
                 </>
             ) : (
                 <>
