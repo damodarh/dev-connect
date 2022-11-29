8578861dcfbf4550456041a8654b2b29bb07b869
Damodar <dhegde50@gmail.com>
2022-11-28 20:39:32 -0600

    Actions for git repo fetching and fetching all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,6 +7,8 @@ import {
     GET_PROFILE,
     PROFILE_ERROR,
     UPDATE_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
 } from './types';
 
 //Get current users profile
@@ -15,6 +17,66 @@ export const getCurrentProfile = () => async (dispatch) => {
         const res = await axios.get('/api/profile/me');
         dispatch({
             type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+//Get all profiles
+export const getProfiles = () => async (dispatch) => {
+    dispatch({
+        type: CLEAR_PROFILE,
+    });
+    try {
+        const res = await axios.get('/api/profile');
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+//Get profile by id
+export const getProfileById = (userId) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+//Get Github repos
+export const geGithubRepos = (userName) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/profile/github/${userName}`);
+        dispatch({
+            type: GET_REPOS,
             payload: res.data,
         });
     } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
     CLEAR_PROFILE,
     GET_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     UPDATE_PROFILE,
 } from '../actions/types';
@@ -19,10 +21,14 @@ export default function (state = initialState, action) {
         case GET_PROFILE:
         case UPDATE_PROFILE:
             return { ...state, profile: payload, loading: false };
+        case GET_PROFILES:
+            return { ...state, profiles: payload, loading: false };
         case PROFILE_ERROR:
             return { ...state, error: payload, loading: false };
         case CLEAR_PROFILE:
             return { ...state, profile: null, repos: [], loading: false };
+        case GET_REPOS:
+            return { ...state, repos: payload, loading: false };
         default:
             return state;
     }
