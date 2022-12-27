e12d9926d9d721c4b3713072bbc02bdd4dedbc6c
Damodar Hegde <dhegde50@gmail.com>
2022-12-27 17:27:59 +0530

    Added post front end code

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,15 +1,31 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
-import connect from "react-redux";
+import { connect } from "react-redux";
 import Spinner from "../Layout/Spinner";
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  
-  return <div>Posts</div>;
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/*Post form */}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
