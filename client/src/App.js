8836dd4b5da995bb0aed8f151d04ae1a4e614f47
Damodar Hegde <dhegde50@gmail.com>
2022-12-27 17:12:52 +0530

    Post redux setup and navlink code added

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -83,6 +84,11 @@ const App = () => {
                                 exact
                                 path='/add-education'
                                 component={AddEducation}
+                            ></PrivateRoute>
+                            <PrivateRoute
+                                exact
+                                path='/posts'
+                                component={Posts}
                             ></PrivateRoute>
                         </section>
                     </Switch>
